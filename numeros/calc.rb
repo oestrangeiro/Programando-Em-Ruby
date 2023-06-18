5fbd958b126885e950314847f43c557c1f938787
oestrangeiro <mateus12092004@gmail.com>
2023-06-17 21:46:12 -0300

    primeiros passos

---
+++ numeros/calc.rb
@@ -0,0 +1,7 @@
+puts 1+3
+puts 3.5+3
+puts -3 * -1
+puts 9/2
+
+puts 'Numero de horas em um ano: '
+puts 365 * 24 #Numero de horas em um ano
---
+++ variaveis/main.rb
@@ -0,0 +1,6 @@
+minhaString = "Gasolina tá cara!"
+precoGasolina = "R$ 5.15"
+
+puts "Quanto ta custando a gasolina?"
+puts "A gasolina tá custando " + precoGasolina + " !"
+puts minhaString
